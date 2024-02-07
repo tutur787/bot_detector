0b3ca1028c3710f33c37f5d3ca50911b9fd0161e
anna-zai1 <125779710+anna-zai1@users.noreply.github.com>
2024-02-07 16:43:47 -0500

    added a function to add the json

--- src/fake_tweets.py
+++ src/fake_tweets.py
@@ -1,4 +1,7 @@
+
 from faker import Faker
+import json
+import random
 
 # Define a function to generate random tweets
 def generate_random_tweets(n):
@@ -16,3 +19,24 @@ def generate_random_tweets(n):
         }
         tweets.append(tweet)
     return tweets
+
+
+def add_tweet(tweet_id, username, text, retweets, likes, timestamp):
+    with open('/Users/annazaidi/Desktop/bot_detector/data/twitter_dataset_2.json', 'r') as file:
+        data = json.load(file)
+        data.append({
+            'Tweet_ID': tweet_id,
+            'Username': username,
+            'Text': text,
+            'Retweets': retweets,
+            'Likes': likes,
+            'Timestamp': timestamp
+        })
+        random.shuffle(data)
+    with open('tweets.json', 'w') as file:
+        json.dump(data, file)
+
+#generate 10 tweeets and add them to /Users/annazaidi/Desktop/bot_detector/data/twitter_dataset_2.json in random places
+tweets = generate_random_tweets(10)
+for tweet in tweets:
+    add_tweet(tweet['Username'], tweet['Text'], tweet['Timestamp'], 0, 0, tweet['Timestamp'])
